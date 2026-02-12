bce8aca654d557b443a6e762126ef0ba501d1fec
denis_tatarinov <skillforce@mail.ru>
2026-02-12 15:41:25 +0300

    return app controller

--- src/setup/swagger.setup.ts
+++ src/setup/swagger.setup.ts
@@ -1,43 +1,22 @@
 import { INestApplication } from '@nestjs/common';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { get } from 'http';
-import { createWriteStream } from 'fs';
 import { CoreConfig } from '../core/configs/core.config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 export const SWAGGER_PREFIX = 'swagger';
+
 export function swaggerSetup(app: INestApplication, coreConfig: CoreConfig) {
-  if (coreConfig.isSwaggerEnabled) {
-    const serverUrl = `http://localhost:${coreConfig.port}`;
-    const config = new DocumentBuilder()
-      .setTitle('WEDDING INVITATION API')
-      .addBearerAuth()
-      .setVersion('1.0.0')
-      .build();
+  if (!coreConfig.isSwaggerEnabled) return;
 
-    const document = SwaggerModule.createDocument(app, config);
-    SwaggerModule.setup(SWAGGER_PREFIX, app, document, {
-      customSiteTitle: 'Blogger platform Swagger',
-    });
-    // write swagger ui files
-    get(`${serverUrl}/api/swagger-ui-bundle.js`, function (response) {
-      response.pipe(createWriteStream('swagger-static/swagger-ui-bundle.js'));
-    });
+  const config = new DocumentBuilder()
+    .setTitle('WEDDING INVITATION API')
+    .setDescription('Wedding Invitation API documentation')
+    .setVersion('1.0.0')
+    .addBearerAuth()
+    .build();
 
-    get(`${serverUrl}/api/swagger-ui-init.js`, function (response) {
-      response.pipe(createWriteStream('swagger-static/swagger-ui-init.js'));
-    });
+  const document = SwaggerModule.createDocument(app, config);
 
-    get(
-      `${serverUrl}/api/swagger-ui-standalone-preset.js`,
-      function (response) {
-        response.pipe(
-          createWriteStream('swagger-static/swagger-ui-standalone-preset.js'),
-        );
-      },
-    );
-
-    get(`${serverUrl}/api/swagger-ui.css`, function (response) {
-      response.pipe(createWriteStream('swagger-static/swagger-ui.css'));
-    });
-  }
+  SwaggerModule.setup(SWAGGER_PREFIX, app, document, {
+    customSiteTitle: 'Wedding API Swagger',
+  });
 }
