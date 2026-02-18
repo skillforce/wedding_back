f5d1848f69acc48251715009fbf0165a94e1446a
denis_tatarinov <skillforce@mail.ru>
2026-02-18 19:13:50 +0300

    CI pipeline

--- src/setup/swagger.setup.ts
+++ src/setup/swagger.setup.ts
@@ -11,7 +11,7 @@ export function swaggerSetup(app: INestApplication, coreConfig: CoreConfig) {
   const config = new DocumentBuilder()
     .setTitle('WEDDING INVITATION API')
     .setDescription('Wedding Invitation API documentation')
-    .setVersion('1.0.0')
+    .setVersion('1.0.1')
     .addBearerAuth()
     .build();
 
